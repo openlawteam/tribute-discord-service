0d1e4d8b531b2f0244d682201521a7c915715611
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2021-08-27 11:58:00 +0100

    organise code

---
+++ src/runners/web3/helpers/subscribeLogs.ts
@@ -0,0 +1,27 @@
+import {Log, LogsOptions} from 'web3-core/types';
+import {Subscription} from 'web3-core-subscriptions';
+
+import {web3} from '../../../alchemyWeb3Instance';
+
+/**
+ * subscribeLogs
+ *
+ * A wrapper to around `web3.eth.subscribe('logs')` with `fromBlock`
+ * set to `latest`, and
+ *
+ * To unsubscribe, call `unsubscribe(callback?)` on the return value.
+ *
+ * @param SubscribeLogsOptions
+ * @returns `Subscription<Log>`
+ * @see https://web3js.readthedocs.io/en/v1.4.0/web3-eth-subscribe.html#subscribe-logs
+ */
+export function subscribeLogs(
+  address: LogsOptions['address'],
+  topics: LogsOptions['topics']
+): Subscription<Log> {
+  return web3.eth.subscribe('logs', {
+    fromBlock: 'latest',
+    address,
+    topics,
+  });
+}
