2f6435a998fd778a15b0a7e8613259e6b26f70e8
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2021-08-27 10:38:04 +0100

    adds sha3 of SponsoredProposal event signature

--- src/registry/web3/eventSignatures.ts
+++ src/registry/web3/eventSignatures.ts
@@ -1,6 +1,13 @@
+import {sha3} from 'web3-utils';
+
 /**
- * Event signature constants
+ * Web3 event signature constants
  */
 
+// @see `abis/SponsoredProposalEvent.json`
 export const SPONSORED_PROPOSAL_EVENT_SIGNATURE: string =
   'SponsoredProposal(bytes32,uint256,address)';
+
+export const SPONSORED_PROPOSAL_EVENT_SIGNATURE_HASH: string | null = sha3(
+  SPONSORED_PROPOSAL_EVENT_SIGNATURE
+);
