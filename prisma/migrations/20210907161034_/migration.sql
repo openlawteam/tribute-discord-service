9ae2ef6c2ac6dd6b72d206f0e17b543b99396cd2
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2021-09-07 17:12:36 +0100

    update initial db schema

--- prisma/migrations/20210907161034_/migration.sql
+++ prisma/migrations/20210907161034_/migration.sql
@@ -1,8 +1,9 @@
 -- CreateTable
-CREATE TABLE "orgs" (
+CREATE TABLE "daos" (
     "id" SERIAL NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "name" VARCHAR(255) NOT NULL,
+    "registryAddress" VARCHAR(42) NOT NULL,
 
     PRIMARY KEY ("id")
 );
@@ -13,16 +14,19 @@ CREATE TABLE "discord_webhooks" (
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "webhookID" VARCHAR(255) NOT NULL,
     "webhookToken" VARCHAR(255) NOT NULL,
-    "orgId" INTEGER NOT NULL,
+    "daoId" INTEGER NOT NULL,
 
     PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "orgs.name_unique" ON "orgs"("name");
+CREATE UNIQUE INDEX "daos.name_unique" ON "daos"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "daos.registryAddress_unique" ON "daos"("registryAddress");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "discord_webhooks.webhookID_unique" ON "discord_webhooks"("webhookID");
 
 -- AddForeignKey
-ALTER TABLE "discord_webhooks" ADD FOREIGN KEY ("orgId") REFERENCES "orgs"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "discord_webhooks" ADD FOREIGN KEY ("daoId") REFERENCES "daos"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- src/webhooks/actions/common/sponsoredProposalAction.ts
+++ src/webhooks/actions/common/sponsoredProposalAction.ts
@@ -1,1 +1,6 @@
+/**
+ * Informs any Discord channels about a sponsored proposal event.
+ *
+ * @returns `Promise<void>`
+ */
 export async function sponsoredProposalAction(): Promise<void> {}
