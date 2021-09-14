f9f5a9e0d1b82879c4cc13129526f6e26a23439b
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2021-09-14 14:42:02 +0100

    cleanup on `sponsoredProposal` template

--- src/webhooks/templates/web3/subscribeLogs/sponsoredProposal.ts
+++ src/webhooks/templates/web3/subscribeLogs/sponsoredProposal.ts
@@ -1,18 +1,16 @@
 // Types
 
-export type SponsoredProposalEmbedTemplateData = Record<
-  'proposalURL' | 'txURL',
-  string
->;
+export type SponsoredProposalEmbedTemplateData = {
+  proposalURL?: string;
+  txURL?: string;
+};
 
 // Templates
 
 export const SPONSORED_PROPOSAL_TEMPLATE: string = `
-**A proposal is up for vote**
-`;
+**A proposal is up for vote**`;
 
 export const SPONSORED_PROPOSAL_EMBED_TEMPLATE: string = `
 **Vote:** {{proposalURL}}
 
-**View transaction:** {{txURL}}
-`;
+**View transaction:** {{txURL}}`;
