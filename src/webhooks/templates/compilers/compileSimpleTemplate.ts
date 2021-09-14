7e2da74e12b0ed249023e4652350db4cd3820b4a
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2021-09-14 14:41:21 +0100

    removes beginning new lines in `compileSimpleTemplate` and adds unit tests

--- src/webhooks/templates/compilers/compileSimpleTemplate.ts
+++ src/webhooks/templates/compilers/compileSimpleTemplate.ts
@@ -1,12 +1,14 @@
 export function compileSimpleTemplate<T>(
   templateString: string,
-  data: T
+  data: T = {} as any
 ): string {
-  let output: string = '';
+  // Remove any new lines at the beginning of the string.
+  templateString = templateString.replace(/^\n{1,}/, '');
 
+  // Replace tokens with values, if any
   Object.entries(data).forEach(([name, value]) => {
-    output = templateString.replace(`{{${name}}}`, value);
+    templateString = templateString.replace(`{{${name}}}`, value);
   });
 
-  return output;
+  return templateString;
 }
