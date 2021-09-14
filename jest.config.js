18dc5e34d80bc4fb711060e24f8ca8bdb7f06f51
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2021-09-14 16:22:42 +0100

    organise jest config

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   clearMocks: true,
   preset: "ts-jest",
-  testEnvironment: "node",
   setupFilesAfterEnv: [
     "<rootDir>/test/prismaMock.ts",
     "<rootDir>/test/setup.ts",
   ],
+  testEnvironment: "node",
   testPathIgnorePatterns: ["<rootDir>/dist/"],
 };
