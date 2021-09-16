a787bd65abba632cce901bc1ea06418e3b9f70e7
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2021-09-16 18:16:28 +0100

    improve `isDebug` test

--- src/helpers/isDebug.unit.test.ts
+++ src/helpers/isDebug.unit.test.ts
@@ -14,6 +14,14 @@ describe('isDebug unit tests', () => {
   });
 
   test('should return `false` when `DEBUG=false`', () => {
+    const originalDEBUG = process.env.DEBUG;
+
+    process.env.DEBUG = 'false';
+
     expect(isDebug()).toBe(false);
+
+    // Cleanup
+
+    process.env.DEBUG = originalDEBUG;
   });
 });
