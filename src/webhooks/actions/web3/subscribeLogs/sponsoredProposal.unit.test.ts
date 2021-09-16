3342102ffb5f32f6a5894f6352242abd6088eaf6
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2021-09-16 12:11:38 +0100

    test improvements for `sponsoredProposal`

--- src/webhooks/actions/web3/subscribeLogs/sponsoredProposal.unit.test.ts
+++ src/webhooks/actions/web3/subscribeLogs/sponsoredProposal.unit.test.ts
@@ -1,26 +1,51 @@
 import {DiscordWebhook} from '@prisma/client';
+import {WebhookClient} from 'discord.js';
 
 import {
   DEFAULT_EMPTY_BYTES32,
   DEFAULT_ETH_ADDRESS,
   FAKE_DAOS,
 } from '../../../../../test';
+import {EventBase, SPONSORED_PROPOSAL_WEB3_LOGS} from '../../../events';
 import {mockWeb3Provider} from '../../../../../test/setup';
 import {prismaMock} from '../../../../../test/prismaMock';
-import {SPONSORED_PROPOSAL_WEB3_LOGS} from '../../../events';
+import {rest, server} from '../../../../../test/msw/server';
 import {sponsoredProposalActionSubscribeLogs} from './sponsoredProposal';
 import {web3} from '../../../../alchemyWeb3Instance';
-import {WebhookClient} from 'discord.js';
-import {rest, server} from '../../../../../test/msw/server';
 
 type MockHelperReturn = Promise<{
   cleanup: () => void;
+  errorHandlerSpy: jest.SpyInstance<
+    void,
+    [
+      {
+        actionName: 'sponsoredProposalSubscribeLogs';
+        event: EventBase;
+        error: Error;
+      }
+    ]
+  >;
   sendSpy?: jest.Mock<any, any>;
   webhookClientMock?: jest.SpyInstance<
     Promise<WebhookClient>,
     [webhookID: string]
   >;
 }>;
+
+const EVENT_DATA = {
+  address: DEFAULT_ETH_ADDRESS,
+  topics: [
+    '0x5b96711deea669ec50fcc8f3d230291ab4711de1d67a0747e6de4ae6c4263d7c',
+  ],
+  data: '0x94fd601ac03ccc41ef4bab53dcd7c0d91b32669408ae5cbe4df972322c2eb47e00000000000000000000000000000000000000000000000000000000000000030000000000000000000000007116c8bebfdf9109aa9d1f188f3025a7f267c095',
+  blockNumber: 9297936,
+  transactionHash:
+    '0x4fc955a5904642c70a0256eac2684ff3cadda36eac919822b565d2f9ba87084e',
+  transactionIndex: 38,
+  blockHash:
+    '0xc4edc2196eb48e1da49c21da02dd7b60ec7435e711a33756e1b9a8befe948cd6',
+  logIndex: 48,
+};
 
 async function mockHelper(
   spyOnWebhookClient: boolean = true
@@ -38,6 +63,15 @@ async function mockHelper(
     webhookToken: 'def456',
     name: 'A Test Webhook',
   };
+
+  // Spy on error logging for test
+
+  const actionErrorHandler = await import('../../helpers/actionErrorHandler');
+
+  const errorHandlerSpy = jest
+    .spyOn(actionErrorHandler, 'actionErrorHandler')
+    // Noop function to remove implementation, i.e. noisy error logs
+    .mockImplementation(() => {});
 
   // Mock result
   prismaMock.discordWebhook.findUnique.mockResolvedValue(webhook);
@@ -75,28 +109,15 @@ async function mockHelper(
     cleanup: () => {
       sendSpy?.mockReset();
       webhookClientMock?.mockRestore();
+      errorHandlerSpy.mockRestore();
     },
+    errorHandlerSpy,
     sendSpy,
     webhookClientMock,
   };
 }
 
 describe('sponsoredProposal unit tests', () => {
-  const EVENT_DATA = {
-    address: DEFAULT_ETH_ADDRESS,
-    topics: [
-      '0x5b96711deea669ec50fcc8f3d230291ab4711de1d67a0747e6de4ae6c4263d7c',
-    ],
-    data: '0x94fd601ac03ccc41ef4bab53dcd7c0d91b32669408ae5cbe4df972322c2eb47e00000000000000000000000000000000000000000000000000000000000000030000000000000000000000007116c8bebfdf9109aa9d1f188f3025a7f267c095',
-    blockNumber: 9297936,
-    transactionHash:
-      '0x4fc955a5904642c70a0256eac2684ff3cadda36eac919822b565d2f9ba87084e',
-    transactionIndex: 38,
-    blockHash:
-      '0xc4edc2196eb48e1da49c21da02dd7b60ec7435e711a33756e1b9a8befe948cd6',
-    logIndex: 48,
-  };
-
   test('should send Discord webhook message', async () => {
     const {cleanup, sendSpy} = await mockHelper();
 
@@ -112,11 +133,6 @@ describe('sponsoredProposal unit tests', () => {
   });
 
   test('should not throw on Discord POST error', async () => {
-    const consoleErrorOriginal = console.error;
-
-    // Turn off logging for test
-    console.error = () => {};
-
     // Mock response error
     server.use(
       rest.post('https://discord.com/api/*/webhooks/*/*', (_req, res, ctx) =>
@@ -124,46 +140,54 @@ describe('sponsoredProposal unit tests', () => {
       )
     );
 
-    const {cleanup, sendSpy} = await mockHelper(false);
+    const {cleanup, errorHandlerSpy, sendSpy} = await mockHelper(false);
+
+    let assertError;
 
     try {
       await sponsoredProposalActionSubscribeLogs(
         SPONSORED_PROPOSAL_WEB3_LOGS,
         FAKE_DAOS
       )(EVENT_DATA);
-
-      // Assert OK
-      expect(sendSpy?.mock.calls).toBe(undefined);
     } catch (error) {
-      // Assert no error thrown
-      expect(error).not.toBeDefined();
+      assertError = error;
     }
+
+    // Assert OK
+    expect(sendSpy?.mock.calls).toBe(undefined);
+    // Assert error logging was called
+    expect(errorHandlerSpy.mock.calls.length).toBe(1);
+    // Assert error was not thrown
+    expect(assertError).not.toBeDefined();
 
     // Cleanup
 
     cleanup();
-
-    console.error = consoleErrorOriginal;
   });
 
   test('should not throw on Web3 call error', async () => {
     // Mock Web3 error
     mockWeb3Provider.injectError({code: 123, message: 'Some bad error.'});
 
-    const {cleanup, sendSpy} = await mockHelper();
+    const {cleanup, errorHandlerSpy, sendSpy} = await mockHelper();
+
+    let assertError;
 
     try {
       await sponsoredProposalActionSubscribeLogs(
         SPONSORED_PROPOSAL_WEB3_LOGS,
         FAKE_DAOS
       )(EVENT_DATA);
-
-      // Assert OK and `WebhookClient.send` not called
-      expect(sendSpy?.mock.calls.length).toBe(0);
     } catch (error) {
-      // Assert no error thrown
-      expect(error).not.toBeDefined();
+      assertError = error;
     }
+
+    // Assert OK and `WebhookClient.send` not called
+    expect(sendSpy?.mock.calls.length).toBe(0);
+    // Assert error logging was called
+    expect(errorHandlerSpy.mock.calls.length).toBe(1);
+    // Assert error was not thrown
+    expect(assertError).not.toBeDefined();
 
     cleanup();
   });
