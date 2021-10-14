cef906f1e05beea9bc5ff9719eb0b93780341e51
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2021-10-14 12:46:38 -0400

    adds `coveragePathIgnorePatterns` to fix coverage

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,11 @@
 module.exports = {
   clearMocks: true,
+  coveragePathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/test/"],
   preset: "ts-jest",
   setupFilesAfterEnv: [
     "<rootDir>/test/prismaMock.ts",
     "<rootDir>/test/setup.ts",
   ],
   testEnvironment: "node",
-  testPathIgnorePatterns: ["<rootDir>/dist/"],
+  testPathIgnorePatterns: ["<rootDir>/dist/", "<rootDir>/test/"],
 };
