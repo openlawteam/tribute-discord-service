570137df7ad61881e1353569da4a8876a2df0adc
Jeremiah Trein <jeremiahtrein@gmail.com>
2021-11-02 14:06:10 +0000

    updates runners registry comment for clarity

--- src/webhook-tasks/runners/registry.ts
+++ src/webhook-tasks/runners/registry.ts
@@ -1,6 +1,6 @@
 /**
- * Export any individual runners which will be executed
- * when the app is run.
+ * Export any individual runners which will be automatically
+ * started when the app is run.
  *
  * Comment-out, or remove, any individual runners not to be run.
  */
