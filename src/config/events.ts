dd822a1fa8c70156fbad9a09605fbbeb523e5f11
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2021-11-03 15:10:34 +0000

    adds event and action enums for snapshot hub

--- src/config/events.ts
+++ src/config/events.ts
@@ -1,4 +1,15 @@
 /**
  * Register unique event names
  */
-export const EVENTS = ['SPONSORED_PROPOSAL'] as const;
+export const EVENTS = [
+  /**
+   * Snapshot Hub
+   */
+  'SNAPSHOT_PROPOSAL_CREATED',
+  'SNAPSHOT_PROPOSAL_END',
+  'SNAPSHOT_PROPOSAL_START',
+  /**
+   * Tribute DAO
+   */
+  'SPONSORED_PROPOSAL',
+] as const;
