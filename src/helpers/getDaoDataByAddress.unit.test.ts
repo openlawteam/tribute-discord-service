fa7cb2a13d2913f8e7944da3e5f506fac8551366
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2021-11-03 15:59:48 +0000

    fixes function name

--- src/helpers/getDaoDataByAddress.unit.test.ts
+++ src/helpers/getDaoDataByAddress.unit.test.ts
@@ -1,16 +1,16 @@
 import {ETH_ADDRESS_FIXTURE, FAKE_DAOS_FIXTURE} from '../../test/fixtures';
-import {getDAODataByAddress} from './getDaoDataByAddress';
+import {getDaoDataByAddress} from './getDaoDataByAddress';
 
 describe('getDaoDataByAddress unit tests', () => {
   test("should return a dao's data", async () => {
-    expect(getDAODataByAddress(ETH_ADDRESS_FIXTURE, FAKE_DAOS_FIXTURE)).toEqual(
+    expect(getDaoDataByAddress(ETH_ADDRESS_FIXTURE, FAKE_DAOS_FIXTURE)).toEqual(
       FAKE_DAOS_FIXTURE.test
     );
   });
 
   test('should return `undefined`', async () => {
     expect(
-      getDAODataByAddress(
+      getDaoDataByAddress(
         '0xf5af0d9c3e4091a48925902eaAB2982e44E7a4C5',
         FAKE_DAOS_FIXTURE
       )
