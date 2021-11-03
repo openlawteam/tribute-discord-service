f567dd42154a840ddf0a12d1bb87eeddb5cf0bca
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2021-11-03 12:23:31 +0000

    handles graceful exit of runners

--- src/webhook-tasks/startWebhookTasks.ts
+++ src/webhook-tasks/startWebhookTasks.ts
@@ -1,9 +1,24 @@
 import {getDaos} from '../services/dao/getDaos';
 import {getEnv} from '../helpers';
-import {runners} from './runners';
+import {RunnerReturn, runners} from './runners';
+
+/**
+ * Handles properly closing any processes started by runners
+ * when Node receives certain signals.
+ */
+const handleExit =
+  (stopRunners: RunnerReturn[]) => async (signal: NodeJS.Signals) => {
+    console.log(`☠️  Received signal to terminate: ${signal}`);
+
+    await Promise.allSettled(stopRunners.map(({stop}) => stop?.()));
+
+    process.exit();
+  };
 
 export async function startWebhookTasks() {
   const daos = await getDaos();
+
+  const stopRunners: RunnerReturn[] = [];
 
   if (!daos) {
     console.warn(`No DAO configuration found for APP_ENV=${getEnv('APP_ENV')}`);
@@ -13,6 +28,15 @@ export async function startWebhookTasks() {
 
   // Execute all registered runners
   Object.values(runners).forEach((r) => {
-    r(daos);
+    // Start runner
+    const runnerResult = r(daos);
+
+    // Save runner results to stop them later
+    stopRunners.push(runnerResult);
   });
+
+  // Handle a graceful exit
+  process.on('SIGINT', handleExit(stopRunners));
+  process.on('SIGTERM', handleExit(stopRunners));
+  process.on('SIGQUIT', handleExit(stopRunners));
 }
