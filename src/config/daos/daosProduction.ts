2195c018465b475af368a54ff3225f80a4a80100
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2021-12-13 16:01:24 +0000

    adds `metaverse`, `red` configs for gov snapshot proposal submit

--- src/config/daos/daosProduction.ts
+++ src/config/daos/daosProduction.ts
@@ -14,7 +14,19 @@ import {DaoData} from '../types';
  * exported mapping below.
  */
 
-export const DAO_NAMES_PRODUCTION = ['muse0'] as const;
+export const DAO_NAMES_PRODUCTION = ['fashion', 'metaverse', 'muse0'] as const;
+
+const SNAPSHOT_HUB_API_URL: string =
+  'https://snapshot-hub-erc712.thelao.io/api';
+
+const DEFAULT_PROPOSAL_RESOLVER = async <R = any>(
+  args: SnapshotHubProposalResolverArgs
+) => {
+  return snapshotHubResolverSelector<R>({
+    ...args,
+    apiBaseURL: SNAPSHOT_HUB_API_URL,
+  });
+};
 
 export const DAOS_PRODUCTION: Record<
   typeof DAO_NAMES_PRODUCTION[number],
@@ -50,15 +62,54 @@ export const DAOS_PRODUCTION: Record<
     friendlyName: 'Muse0',
     registryContractAddress: '0x7c8B281C56f7ef9b8099D3F491AF24DC2C2e3ee0',
     snapshotHub: {
-      proposalResolver: async <R = any>(
-        args: SnapshotHubProposalResolverArgs
-      ) => {
-        return snapshotHubResolverSelector<R>({
-          ...args,
-          apiBaseURL: 'https://snapshot-hub-erc712.thelao.io/api',
-        });
-      },
+      proposalResolver: DEFAULT_PROPOSAL_RESOLVER,
       space: 'museo',
+    },
+  },
+
+  fashion: {
+    actions: [
+      {
+        name: 'SNAPSHOT_PROPOSAL_CREATED_WEBHOOK',
+        webhookID: '919977631540207646',
+      },
+    ],
+    adapters: {
+      [BURN_ADDRESS]: {
+        friendlyName: 'Governance',
+        baseURLPath: 'governance',
+      },
+    },
+    baseURL: 'https://reddao.xyz',
+    events: [{name: 'SNAPSHOT_PROPOSAL_CREATED'}],
+    friendlyName: 'Red DAO',
+    registryContractAddress: '0x1D96d039d384d3ECCaD6f07aAB27A49408A1Cf2B',
+    snapshotHub: {
+      proposalResolver: DEFAULT_PROPOSAL_RESOLVER,
+      space: 'fashion',
+    },
+  },
+
+  metaverse: {
+    actions: [
+      {
+        name: 'SNAPSHOT_PROPOSAL_CREATED_WEBHOOK',
+        webhookID: '919978975059673138',
+      },
+    ],
+    adapters: {
+      [BURN_ADDRESS]: {
+        friendlyName: 'Governance',
+        baseURLPath: 'governance',
+      },
+    },
+    baseURL: 'https://neondao.xyz',
+    events: [{name: 'SNAPSHOT_PROPOSAL_CREATED'}],
+    friendlyName: 'NEON DAO',
+    registryContractAddress: '0xa9D57fB58926d498c792733ee86d3Cc8EB8bb7A0',
+    snapshotHub: {
+      proposalResolver: DEFAULT_PROPOSAL_RESOLVER,
+      space: 'metaverse',
     },
   },
 };
