ec21d7a22f6c6229ace7cb9061b95a51523f0d06
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2022-02-17 17:34:05 +0000

    adds comment to migration about uuid

--- prisma/migrations/20220217161920_/migration.sql
+++ prisma/migrations/20220217161920_/migration.sql
@@ -1,11 +1,21 @@
 /*
+  Enable the use of uuid functions, if it's not enabled already.
+
+  Prisma does not currently automatically enable `uuid-ossp` if
+  uuid functions are used in the schema.
+
+  @see https://github.com/prisma/prisma/issues/6822
+*/
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+/*
   Warnings:
 
   - Made the column `uuid` on table `buy_nft_poll` required. This step will fail if there are existing NULL values in that column.
   - Made the column `uuid` on table `floor_sweeper_poll` required. This step will fail if there are existing NULL values in that column.
 
 */
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 -- AlterTable
 ALTER TABLE "buy_nft_poll" ALTER COLUMN "uuid" SET NOT NULL,
