fe389b7abb64eb2a62687d9bcac72349e4f21e9a
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2022-02-18 14:19:10 +0000

    `/fund` initial development (#82)

---
+++ src/applications/tribute-tools/commands/fund.unit.test.ts
@@ -0,0 +1,291 @@
+import {
+  Client,
+  ClientOptions,
+  CommandInteraction,
+  Intents,
+  InteractionReplyOptions,
+} from 'discord.js';
+import {GatewayInteractionCreateDispatchData} from 'discord-api-types';
+
+import {BUY_ALLOWED_EMOJIS} from '../config';
+import {
+  ETH_ADDRESS_FIXTURE,
+  FAKE_DAOS_FIXTURE,
+  GUILD_ID_FIXTURE,
+} from '../../../../test';
+import {fund} from './fund';
+
+describe('fund unit tests', () => {
+  const CLIENT_OPTIONS: ClientOptions = {
+    intents: [
+      Intents.FLAGS.GUILDS,
+      Intents.FLAGS.GUILD_MESSAGES,
+      Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+    ],
+    partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
+    restSweepInterval: 0,
+  };
+
+  const INTERACTION_OPTIONS = [
+    {name: 'address_to_fund', type: 3, value: ETH_ADDRESS_FIXTURE},
+    {name: 'amount_in_usdc', type: 4, value: 50},
+    {
+      name: 'purpose',
+      type: 3,
+      value: 'Seed round for Tribute Labs',
+    },
+  ];
+
+  const INTERACTION_DATA: GatewayInteractionCreateDispatchData = {
+    /**
+     * ID of the interaction
+     */
+    id: 'abc2354',
+    /**
+     * ID of the application this interaction is for
+     */
+    application_id: '931125521645969448',
+    /**
+     * The type of interaction
+     */
+    type: 2,
+    /**
+     * The command data payload
+     */
+    data: {
+      id: '123456789',
+      name: 'fund',
+      options: INTERACTION_OPTIONS,
+    },
+
+    /**
+     * The guild it was sent from
+     */
+    guild_id: GUILD_ID_FIXTURE,
+    /**
+     * The channel it was sent from
+     */
+    channel_id: '886976610018934824',
+
+    /**
+     * A continuation token for responding to the interaction
+     */
+    token:
+      'aW50ZXJhY3Rpb246OTMzOTI4Mjc2MjIzNzI5Njc1OlZUT2xKd3kzOFZOWnlEcEIxczFvbjA2aXZabldkUjRQRFVVOVA5VlNVc3lxSk5QTmtQbjhuQmRmajdvaG1ZMzgyTEN6ejU4Y0VUbTV3UGkyZE16VTM3MXRXQTJIa0xISFphTkhaS1ltSGdNNVp5a2UybW5CVzZ3a0tKR1lIaFZl',
+    /**
+     * Read-only property, always `1`
+     */
+    version: 1,
+    user: {
+      id: '718496526103478282',
+      bot: false,
+      system: false,
+      username: 'test',
+      discriminator: '8617',
+      avatar: 'd2b72ef9b0fc65a89a975faeb4a3a916',
+    },
+  };
+
+  test('should run execute', async () => {
+    const client = new Client(CLIENT_OPTIONS);
+    const interaction = new CommandInteraction(client, INTERACTION_DATA);
+    const reactSpy = jest.fn();
+
+    const interactionReplySpy = jest
+      .spyOn(interaction, 'reply')
+      .mockImplementation(
+        async (_o) =>
+          (await {guildId: GUILD_ID_FIXTURE, react: reactSpy}) as any
+      );
+
+    // Mock `getDaos`
+    const getDaosSpy = jest
+      .spyOn(await import('../../../services/dao/getDaos'), 'getDaos')
+      .mockImplementation(async () => FAKE_DAOS_FIXTURE);
+
+    const fundResult = await fund.execute(interaction);
+
+    expect(fundResult).toBe(undefined);
+    expect(interactionReplySpy).toHaveBeenCalledTimes(1);
+
+    expect(
+      (interactionReplySpy.mock.calls[0][0] as InteractionReplyOptions)
+        .embeds?.[0]?.title
+    ).toMatch(/Seed round for Tribute Labs/i);
+
+    expect(
+      (interactionReplySpy.mock.calls[0][0] as InteractionReplyOptions)
+        .embeds?.[0]?.url
+    ).toBe(`https://etherscan.io/address/${ETH_ADDRESS_FIXTURE}`);
+
+    expect(
+      (interactionReplySpy.mock.calls[0][0] as InteractionReplyOptions)
+        .embeds?.[0]?.description
+    ).toMatch(
+      /⚠️ 🤖 is in development\.\n\n📊 \*\*Should we fund `0x04028Df0Cea639E97fDD3fC01bA5CC172613211D` for \$50\.00 USDC\?\*\*/i
+    );
+
+    expect(
+      (interactionReplySpy.mock.calls[0][0] as InteractionReplyOptions)
+        .embeds?.[0]?.fields?.[0].name
+    ).toMatch(/vote threshold/i);
+
+    expect(
+      (interactionReplySpy.mock.calls[0][0] as InteractionReplyOptions)
+        .embeds?.[0]?.fields?.[0].value
+    ).toMatch(/3 upvotes/i);
+
+    expect(reactSpy).toHaveBeenCalledTimes(2);
+    expect(reactSpy).toHaveBeenNthCalledWith(1, BUY_ALLOWED_EMOJIS[0]);
+    expect(reactSpy).toHaveBeenNthCalledWith(2, BUY_ALLOWED_EMOJIS[1]);
+
+    // Cleanup
+
+    getDaosSpy.mockRestore();
+    interactionReplySpy.mockRestore();
+  });
+
+  test('should not run execute if interaction not command', async () => {
+    const client = new Client(CLIENT_OPTIONS);
+
+    const interaction = new CommandInteraction(client, {
+      ...INTERACTION_DATA,
+      type: 1,
+    });
+
+    const reactSpy = jest.fn();
+
+    const interactionReplySpy = jest
+      .spyOn(interaction, 'reply')
+      .mockImplementation(
+        async (_o) =>
+          (await {guildId: '722525233755717762', react: reactSpy}) as any
+      );
+
+    const fundResult = await fund.execute(interaction);
+
+    expect(fundResult).toBe(undefined);
+    expect(interactionReplySpy.mock.calls.length).toBe(0);
+    expect(reactSpy.mock.calls.length).toBe(0);
+
+    // Cleanup
+
+    interactionReplySpy.mockRestore();
+  });
+
+  test('should reply with error if `address_to_fund` option is not valid', async () => {
+    const client = new Client(CLIENT_OPTIONS);
+
+    const interaction = new CommandInteraction(client, {
+      ...INTERACTION_DATA,
+      data: {
+        ...INTERACTION_DATA.data,
+        options: [{name: 'address_to_fund', type: 3, value: '0x0'}],
+      },
+    });
+
+    const reactSpy = jest.fn();
+
+    const interactionReplySpy = jest
+      .spyOn(interaction, 'reply')
+      .mockImplementation(
+        async (_o) =>
+          (await {guildId: '722525233755717762', react: reactSpy}) as any
+      );
+
+    const fundResult = await fund.execute(interaction);
+
+    expect(fundResult).toBe(undefined);
+    expect(interactionReplySpy.mock.calls.length).toBe(1);
+
+    expect(interactionReplySpy.mock.calls[0][0]).toEqual({
+      content:
+        'Invalid Ethereum address. Try something like: 0x000000000000000000000000000000000000bEEF.',
+      ephemeral: true,
+    });
+
+    // Cleanup
+
+    interactionReplySpy.mockRestore();
+  });
+
+  test('should reply with error if DAO is not found', async () => {
+    const client = new Client(CLIENT_OPTIONS);
+    const interaction = new CommandInteraction(client, INTERACTION_DATA);
+    const reactSpy = jest.fn();
+
+    // Mock `getDaos`
+    const getDaosSpy = jest
+      .spyOn(await import('../../../services/dao/getDaos'), 'getDaos')
+      // Insert bad guild ID so the dao is not found
+      .mockImplementation(async () => ({
+        ...FAKE_DAOS_FIXTURE,
+        test: {...FAKE_DAOS_FIXTURE.test, guildID: '00000000000000'},
+      }));
+
+    const interactionReplySpy = jest
+      .spyOn(interaction, 'reply')
+      .mockImplementation(
+        async (_o) =>
+          (await {guildId: '722525233755717762', react: reactSpy}) as any
+      );
+
+    await fund.execute(interaction);
+
+    expect(interactionReplySpy.mock.calls[0][0]).toEqual({
+      content: 'Something went wrong while setting up the poll.',
+      ephemeral: true,
+    });
+
+    // Cleanup
+
+    getDaosSpy.mockRestore();
+    interactionReplySpy.mockRestore();
+  });
+
+  test('should reply with error and delete poll if no `guildId` returned from `reply`', async () => {
+    const client = new Client(CLIENT_OPTIONS);
+    const interaction = new CommandInteraction(client, INTERACTION_DATA);
+    const deleteSpy = jest.fn();
+    const reactSpy = jest.fn();
+
+    // Mock `getDaos`
+    const getDaosSpy = jest
+      .spyOn(await import('../../../services/dao/getDaos'), 'getDaos')
+      .mockImplementation(async () => FAKE_DAOS_FIXTURE);
+
+    const interactionReplySpy = jest
+      .spyOn(interaction, 'reply')
+      .mockImplementation(
+        async (_o) =>
+          // Do not return a `guildID`
+          (await {
+            delete: deleteSpy,
+            react: reactSpy,
+          }) as any
+      );
+
+    const interactionFollowUpSpy = jest
+      .spyOn(interaction, 'followUp')
+      .mockImplementation(async (_o) => (await {}) as any);
+
+    await fund.execute(interaction);
+
+    expect(interactionReplySpy).toHaveBeenCalledTimes(1);
+    expect(reactSpy).toHaveBeenCalledTimes(0);
+    expect(interactionFollowUpSpy).toHaveBeenCalledTimes(1);
+
+    expect(interactionFollowUpSpy).toHaveBeenNthCalledWith(1, {
+      content: 'Something went wrong while setting up the poll.',
+      ephemeral: true,
+    });
+
+    expect(deleteSpy.mock.calls.length).toBe(1);
+
+    // Cleanup
+
+    getDaosSpy.mockRestore();
+    interactionFollowUpSpy.mockRestore();
+    interactionReplySpy.mockRestore();
+  });
+});
