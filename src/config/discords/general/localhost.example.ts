f46cd1fae89182626a58eda0cfc4a342e05ea00b
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2022-04-01 11:21:07 +0100

    adds example localhost file for general discords

---
+++ src/config/discords/general/localhost.example.ts
@@ -0,0 +1,33 @@
+/**
+ * ----------------------------------------
+ * INSTRUCTIONS
+ *
+ * If you want to test the app in localhost
+ * copy the contents of this file
+ * to `localhost.ts`.
+ * ----------------------------------------
+ */
+
+/*
+import {DiscordConfig} from '../../types';
+
+const LOCALHOST_DISCORD_NAMES = ['tributeLabs'] as const;
+
+export const LOCALHOST_DISCORD_CONFIGS: Record<
+  typeof LOCALHOST_DISCORD_NAMES[number],
+  DiscordConfig
+> = {
+  tributeLabs: {
+    actions: [
+      {
+        name: 'TRIBUTE_TOOLS_ADMIN_FEE_WEBHOOK',
+        webhookID: '886976872611729439',
+      },
+    ],
+    baseURL: 'https://tributelabs.xyz',
+    events: [],
+    friendlyName: 'Tribute Labs [DEV]',
+    guildID: '722525233755717762',
+  },
+};
+*/
